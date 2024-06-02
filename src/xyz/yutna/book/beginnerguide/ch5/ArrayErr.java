72dbcc00f018408a45e7796763a92626edf72da0
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-06-02 21:11:35 +0700

    Backup Chapter 5 - More Data Types and Operators

---
+++ src/xyz/yutna/book/beginnerguide/ch5/ArrayErr.java
@@ -0,0 +1,14 @@
+package xyz.yutna.book.beginnerguide.ch5;
+
+// Demonstrate an array overrun.
+public class ArrayErr {
+    public static void main(String[] args) {
+        int[] sample = new int[10];
+        int i;
+
+        // Generate an array overrun
+        for (i = 0; i < 100; i = i + 1) {
+            sample[i] = i;
+        }
+    }
+}
