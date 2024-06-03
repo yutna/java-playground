8d012ca0a6930cda7a813b8f2432df77d0aac44a
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-06-03 22:05:49 +0700

    Backup Chapter 5 - More Date Types and Operators #4

---
+++ src/xyz/yutna/books/abeginnersguide/ch5/NotDemo.java
@@ -0,0 +1,28 @@
+package xyz.yutna.books.abeginnersguide.ch5;
+
+class NotDemo {
+    public static void main(String[] args) {
+        byte b = -34;
+
+        for (int t = 128; t > 0; t = t / 2) {
+            if ((b & t) != 0)
+                System.out.print("1 ");
+            else
+                System.out.print("0 ");
+        }
+
+        System.out.println();
+
+        // Reverse all bits
+        b = (byte) ~b;
+
+        for (int t = 128; t > 0; t = t / 2) {
+            if ((b & t) != 0)
+                System.out.print("1 ");
+            else
+                System.out.print("0 ");
+        }
+
+        System.out.println();
+    }
+}
