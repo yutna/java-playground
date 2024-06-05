03cff40980405c29e44823d5b4197c4832a8c3f3
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-06-06 06:12:32 +0700

    Backup Chapter 6 - A Closer look at Methods and Classes #4

---
+++ src/xyz/yutna/books/abeginnersguide/ch6/VarArgs.java
@@ -0,0 +1,30 @@
+package xyz.yutna.books.abeginnersguide.ch6;
+
+// NOTE - Varargs can be called with zero or more argument.
+// Futuremore, it causes v to be implicitly declared as an array of type int[].
+// Thus, inside vaTest(), v is accessed using the normal array syntax.
+
+// NOTE - A method can have `normal` parameters along with a variable-length parameter.
+// However, the variable-length parameter must be the last parameter declared by the
+// method.
+// int doIt(int a, int b, double c, int... vals)
+// LINK - ./VarArgs2.java
+
+// Demonstrate variable-length arguments.
+class VarArgs {
+    public static void main(String[] args) {
+        // Notice how vaTest() can be called with a variable number of arguments.
+        vaTest(10); // 1 arg
+        vaTest(1, 2, 3); // 3 args
+        vaTest(); // no args
+    }
+
+    static void vaTest(int... v) {
+        System.out.println("Number of args: " + v.length);
+        System.out.println("Contents: ");
+
+        for (int i = 0; i < v.length; i++)
+            System.out.println(" arg " + i + ": " + v[i]);
+        System.out.println();
+    }
+}
---
+++ src/xyz/yutna/books/abeginnersguide/ch6/VarArgs2.java
@@ -0,0 +1,23 @@
+package xyz.yutna.books.abeginnersguide.ch6;
+
+// NOTE - Remember, the varargs parameter must be last.
+// NOTE - There is one more restriction to be aware of: there must be only **one** varargs parameter.
+
+// Use varargs with standard arguments.
+class VarArgs2 {
+    public static void main(String[] args) {
+        vaTest("One vararg: ", 10);
+        vaTest("Three varargs: ", 1, 2, 3);
+        vaTest("No varargs: ");
+    }
+
+    // Here, msg is a normal parameter and v is a varargs parameter.
+    static void vaTest(String msg, int... v) {
+        System.out.println(msg + v.length);
+        System.out.println("Contents: ");
+
+        for (int i = 0; i < v.length; i++)
+            System.out.println("  arg " + i + ": " + v[i]);
+        System.out.println();
+    }
+}
